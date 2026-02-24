3d4d576f6b44106b146fb8fcf39f1603f65d6fb1
Ramiro Romera <externo.ramiroromera+azar@allianz.com.ar>
2026-02-24 14:36:15 -0300

    Migrate to Java 21

--- src/test/java/ar/edu/utn/frc/tup/lciii/configs/SpringDocConfigTest.java
+++ src/test/java/ar/edu/utn/frc/tup/lciii/configs/SpringDocConfigTest.java
@@ -1,12 +1,14 @@
 package ar.edu.utn.frc.tup.lciii.configs;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.ResponseEntity;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -18,17 +20,21 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SpringDocConfigTest {
 
-    @Autowired
-    private TestRestTemplate restTemplate;
+    @LocalServerPort
+    private int port;
 
     @Test
-    void getDocumentation() throws IOException {
-        ResponseEntity<String> responseEntity = this.restTemplate.getForEntity("/v3/api-docs", String.class);
-        assertTrue(responseEntity.getStatusCode().is2xxSuccessful());
-        assertNotNull(responseEntity.getBody());
+    void getDocumentation() throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        URI uri = URI.create("http://localhost:" + this.port + "/v3/api-docs");
+        HttpRequest request = HttpRequest.newBuilder(uri).GET().build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        assertTrue(response.statusCode() >= 200 && response.statusCode() < 300);
+        assertNotNull(response.body());
 
         Path specs = Paths.get("docs/specs");
         Files.createDirectories(specs);
-        Files.writeString(specs.resolve("swagger.json"), responseEntity.getBody());
+        Files.writeString(specs.resolve("swagger.json"), response.body());
     }
 }
--- src/test/java/ar/edu/utn/frc/tup/lciii/controllers/PingControllerTest.java
+++ src/test/java/ar/edu/utn/frc/tup/lciii/controllers/PingControllerTest.java
@@ -1,10 +1,12 @@
 package ar.edu.utn.frc.tup.lciii.controllers;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -13,11 +15,17 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-@AutoConfigureMockMvc
 public class PingControllerTest {
 
     @Autowired
+    private WebApplicationContext webApplicationContext;
+
     private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUpMockMvc() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
+    }
 
     @Test
     public void pingTest() throws Exception {
